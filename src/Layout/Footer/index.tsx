413dcf3f63f860f151f4bc8559e5753f48e4f857
Dmitric <mr_vlasov@mail.ru>
2023-08-22 22:21:46 +0300

    works with map

--- src/Layout/Footer/index.tsx
+++ src/Layout/Footer/index.tsx
@@ -29,7 +29,7 @@ export const Footer = ({ className }:indexProps): JSX.Element => {
 	return (
 		<div className={className}>
 			<div>Footer</div>
-			<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ac065749e88ff9b0bffa12601006ecf84ae8466a3e52c6bc98203cd545ad5fdca&amp;source=constructor" width="341" height="262" ></iframe>
+			<iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3Ac065749e88ff9b0bffa12601006ecf84ae8466a3e52c6bc98203cd545ad5fdca&amp;source=constructor" width="360" height="289" ></iframe>
 		</div>
 	)
 }
