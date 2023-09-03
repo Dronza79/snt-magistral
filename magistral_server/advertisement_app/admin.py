3206d36d2ba91a00a36960db775ed79171fa496d
Зайцев Андрей <mr_zaycev@mail.ru>
2023-09-03 20:57:11 +0300

    поправил

--- magistral_server/advertisement_app/admin.py
+++ magistral_server/advertisement_app/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+def register_hidden_models(*model_names):
+    for model in model_names:
+        model_admin = type(
+            str(model)+'Admin',
+            (admin.ModelAdmin,),
+            {
+                'get_model_perms': lambda self, request: {}
+            })
+        admin.site.register(model, model_admin)
+
+
+register_hidden_models(FileNews)
+
+
+@admin.register(Advertisement)
+class AdvertisementAdmin(admin.ModelAdmin):
+    ordering = ['pub_date']
+    list_filter = ['tag_news']
+    list_display = []
--- magistral_server/advertisement_app/models.py
+++ magistral_server/advertisement_app/models.py
@@ -1,16 +1,35 @@
 from django.db import models
 
 
-class News(models.Model):
+class Advertisement(models.Model):
     TAG_CHO = [
         ('notification', 'Уведомление'),
         ('important', 'Важно'),
     ]
     create_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
-    edit_at = models.DateTimeField(auto_now=True, verbose_name='')
+    edit_at = models.DateTimeField(auto_now=True, verbose_name='Дата редактирования')
     pub_date = models.DateTimeField('Опубликовано', blank=True, null=True)
     editor = models.CharField('Отредактировал', max_length=255, blank=True)
-    autor = models.CharField('Автор', max_length=255)
+    autor = models.CharField('Автор', max_length=255, default='Правление СНТ')
     tag_news = models.CharField(max_length=12, choices=TAG_CHO, verbose_name="Пометка", blank=True, default="")
-    title_news = models.CharField('Заголовок новости', max_length=255)
+    title_news = models.CharField('Заголовок объявления', max_length=255)
     content_news = models.TextField()
+
+    def __str__(self):
+        return f'news{self.create_at}'
+
+    class Meta:
+        ordering = ['pub_date']
+        verbose_name = 'Объявление'
+        verbose_name_plural = 'Объявления'
+
+
+def get_file_storage_path(inst, filename):
+    return f'{inst.news}/{filename}'
+
+
+class FileNews(models.Model):
+    file_name = models.CharField(verbose_name='Название файла', max_length=255)
+    file_descr = models.CharField(verbose_name='Описание файла', max_length=255, blank=True)
+    news = models.ForeignKey(Advertisement, related_name='file_news', on_delete=models.CASCADE)
+    file = models.FileField(upload_to=get_file_storage_path)
