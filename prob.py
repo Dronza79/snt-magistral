c1b27c3d17c7e4556e483182ece4cf7cb15f2775
Зайцев Андрей <mr_zaycev@mail.ru>
2023-09-06 00:13:12 +0300

    добавил левый файл

--- prob.py
+++ prob.py
@@ -3,34 +3,25 @@ class Person:
         self.name = name
         self.age = age
         self.friends = []
-        self.requrs_stop = 0
+        self.stop_req = 0
 
-    def __repr__(self):
+    def __str__(self):
         return self.name
 
-    # def to_dist(self):
-    #     return {
-    #         'name': self.name,
-    #         'age': self.age,
-    #         'friends': [person.to_dist() for person in self.friends]
-    #     }
-
     def to_dist(self):
-        self.requrs_stop += 1
-        if self.requrs_stop > 1:
-            return f'Сам {self.name}'
-        else:
-            return {
-                'name': self.name,
-                'age': self.age,
-                'friends': [person.to_dist() for person in self.friends]
-            }
+        self.stop_req += 1
+        if self.stop_req > 2:
+            # self.stop_req = False
+            return f'сам {self}'
+        return {
+            'name': self.name,
+            'age': self.age,
+            'friends': [person.to_dist() for person in self.friends]
+        }
 
     def add_friend(self, obj):
         if obj not in self.friends:
             self.friends.append(obj)
-            # print(self)
-            # print(obj)
             obj.add_friend(self)
         else:
             return 'Уже друзья'
@@ -42,7 +33,7 @@ rob = Person('Роб', '11')
 
 bob.add_friend(rob)
 rob.add_friend(nik)
-# nik.add_friend(bob)
+nik.add_friend(bob)
 
 # print(bob.__dict__)
 # print(nik.__dict__)
@@ -50,15 +41,19 @@ rob.add_friend(nik)
 print(bob.to_dist())
 
 TIP = {
-    'name': 'Роб',
-    'age': '11',
+    'name': 'Боб',
+    'age': '12',
     'friends': [
-        {
-            'name': 'Боб',
-            'age': '12',
-            'friends': [
-                None,
-                {
-                    'name': 'Ник',
-                    'age': '13',
-                    'friends': [None]}]}]}
+        {'name': 'Роб',
+         'age': '11',
+         'friends': [
+             'сам Боб',
+             {'name': 'Ник',
+              'age': '13',
+              'friends': [
+                  'сам Роб'
+              ]}
+         ]}
+    ]}
+
+di = {'name': 'Боб', 'age': '12', 'friends': [{'name': 'Роб', 'age': '11', 'friends': ['сам Боб', {'name': 'Ник', 'age': '13', 'friends': ['сам Роб', 'сам Боб']}]}, 'сам Ник']}
