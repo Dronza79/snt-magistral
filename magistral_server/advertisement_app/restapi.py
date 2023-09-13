8eac951d7f4ae7cd6cb7e346ffd7b81f83432e41
Зайцев Андрей <mr_zaycev@mail.ru>
2023-09-13 21:25:37 +0300

    починил вывод списка и отдельной новости

--- magistral_server/advertisement_app/restapi.py
+++ magistral_server/advertisement_app/restapi.py
@@ -4,7 +4,14 @@ from .models import Advertisement
 from .serializer import AdvertisementSerializer
 
 
-class NewsAPIView(RetrieveAPIView, ListAPIView):
+class ListNewsAPIView(ListAPIView):
+    serializer_class = AdvertisementSerializer
+
+    def get_queryset(self):
+        return Advertisement.objects.filter(state_news__icontains='public')
+
+
+class DetailNewsAPIView(RetrieveAPIView):
     serializer_class = AdvertisementSerializer
 
     def get_queryset(self):
--- magistral_server/advertisement_app/urls.py
+++ magistral_server/advertisement_app/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .restapi import *
 
 urlpatterns = [
-    path('api/news/', NewsAPIView.as_view(), name='news_list'),
-    path('api/news/<int:pk>/', NewsAPIView.as_view(), name='detail_news'),
+    path('api/news/', ListNewsAPIView.as_view(), name='news_list'),
+    path('api/news/<int:pk>/', DetailNewsAPIView.as_view(), name='detail_news'),
 ]
