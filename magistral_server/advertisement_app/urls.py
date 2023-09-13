dd39f974cb58e15b133f22fadf9949f6523e38c2
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2023-09-13 11:06:05 +0300

    Добавил выдачу детальной информации о новости

--- magistral_server/advertisement_app/urls.py
+++ magistral_server/advertisement_app/urls.py
@@ -4,4 +4,5 @@ from .restapi import *
 
 urlpatterns = [
     path('api/news/', NewsAPIView.as_view(), name='news_list'),
+    path('api/news/<int:pk>/', NewsAPIView.as_view(), name='detail_news'),
 ]
