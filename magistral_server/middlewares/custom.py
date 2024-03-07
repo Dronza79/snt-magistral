76f131d8b426e36ba22946ebc42d40a66aab66da
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2024-03-07 13:53:39 +0300

    Добавил автозакрытие протоколов

---
+++ magistral_server/middlewares/custom.py
@@ -0,0 +1,19 @@
+from django.urls import reverse
+from django.utils import timezone
+
+from voting_app.models import MeetingProtocol
+
+
+class CheckMeetingProtocolsMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        obj = MeetingProtocol
+        if request.path in (reverse('protocols_list'),
+                            f'/admin/{obj._meta.app_label}/{obj._meta.model_name}/'):
+            (obj.objects
+             .filter(close_event__lt=timezone.now(), status='open')
+             .update(status='close'))
+        response = self.get_response(request)
+        return response
