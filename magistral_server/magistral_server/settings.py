a32fb29a6c5bd9103f972f3553a9472b9b2a46ae
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2024-03-07 13:54:23 +0300

    Добавил автозакрытие протоколов

--- magistral_server/magistral_server/settings.py
+++ magistral_server/magistral_server/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'middlewares.custom.CheckMeetingProtocolsMiddleware',
 ]
 
 ROOT_URLCONF = 'magistral_server.urls'
