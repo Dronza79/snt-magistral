4599c3972541b1ecda060f2961a52ff286581190
Dronza79 <113519920+Dronza79@users.noreply.github.com>
2024-03-11 18:19:25 +0300

    Update urls.py

--- magistral_server/forum_app/urls.py
+++ magistral_server/forum_app/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from .restapi import *
 
 urlpatterns = [
-    path('api/post/list/', ListPostView.as_view(), name='list_post'),
     path('api/post/add/', CreatePostView.as_view(), name='create_post'),
+    path('api/post/list/', ListPostView.as_view(), name='list_posts'),
     path('api/post/<int:pk>/', DetailPostView.as_view(), name='detail_post'),
     # path('api/voting/vote/', CreateVotesView.as_view(), name='get_votes'),
 ]
